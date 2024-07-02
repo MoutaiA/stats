27dd31a417b53a88aa8ec4197fcc332158df726e
amoutai <amin.moutai@gmail.com>
2024-07-02 22:03:53 +0200

    compounding datas

--- src/Strava.js
+++ src/Strava.js
@@ -6,19 +6,29 @@ class Strava {
 		this.accessToken = process.env.STRAVA_ACCESS_TOKEN;
 	}
 
-	getOptions() {
+	async getData() {
+		const before = new Date();
+		// the CRON JOB is run every sunday at night => so we need to get the last 2 weeks activities => today - 14
+		const mondayLastWeek = new Date();
+		const after = mondayLastWeek.setDate(mondayLastWeek.getDate() - 14);
+
+		const activities = await this.getActivities(before, after);
+		return this.#compute(activities);
+	}
+
+	async getActivities(before, after) {
+		const URL = encodeURI(`${this.baseUrl}/athlete/activites?before=${before}&after=${after}`);
+		let result = await fetch(URL, this.#getOptions());
+		result = await result.json();
+		return this.#format(result);
+	}
+
+	#getOptions() {
 		return {
 			headers: {
 				Authorization: `Bearer ${this.accessToken}`,
 			},
 		};
-	}
-
-	async getActivities(before, after) {
-		const URL = encodeURI(`${this.baseUrl}/athlete/activites?before=${before}&after=${after}`);
-		let result = await fetch(URL, this.getOptions());
-		result = await result.json();
-		return this.#format(result);
 	}
 
 	#format(data) {
@@ -31,6 +41,21 @@ class Strava {
 			});
 		}
 		return results;
+	}
+
+	#compute(activities) {
+		let distance = 0;
+		let moving_time = 0;
+
+		for (const activity of activities) {
+			distance += activity.distance;
+			moving_time += activity.moving_time;
+		}
+
+		return {
+			distance,
+			moving_time,
+		};
 	}
 }
 
--- tests/unit/Strava.test.js
+++ tests/unit/Strava.test.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 
 jest.mock('node-fetch', () => jest.fn());
 
-describe('Mock implementation of getActivities', () => {
+describe('Mock getData', () => {
 	beforeAll(() => {
 		fetch.mockImplementation(() =>
 			Promise.resolve({
@@ -13,23 +13,37 @@ describe('Mock implementation of getActivities', () => {
 		);
 	});
 
-	test('Format the result', async () => {
-		const expected = [
-			{
-				id: 154504250376823,
-				distance: 24931.4,
-				moving_time: 4500,
-			},
-			{
-				id: 1234567809,
-				distance: 23676.5,
-				moving_time: 5400,
-			},
-		];
+	test('getData', async () => {
+		const expected = {
+			distance: 48607.9,
+			moving_time: 9900,
+		};
 		const strava = new Strava();
 
-		const result = await strava.getActivities(1, 2);
+		const result = await strava.getData();
 
 		expect(result).toEqual(expected);
+	});
+
+	describe('Mock implementation of getActivities', () => {
+		test('Format the result', async () => {
+			const expected = [
+				{
+					id: 154504250376823,
+					distance: 24931.4,
+					moving_time: 4500,
+				},
+				{
+					id: 1234567809,
+					distance: 23676.5,
+					moving_time: 5400,
+				},
+			];
+			const strava = new Strava();
+
+			const result = await strava.getActivities(1, 2);
+
+			expect(result).toEqual(expected);
+		});
 	});
 });
