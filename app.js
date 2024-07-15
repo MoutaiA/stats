7c3642d1999ad8c84dd07180f096bcaa7b55d356
amoutai <amin.moutai@gmail.com>
2024-07-15 22:20:58 +0200

    feat: using route for front

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require('express');
+const cors = require('cors');
+
 const Strava = require('./src/StravaAPI');
 const RefreshToken = require('./src/RefreshToken');
 const ComputationService = require('./src/ComputationService');
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.json({ clientID: process.env.STRAVA_CLIENT_ID });
+});
+
+app.get('/exchange_token', (req, res) => {
+	console.log(req.body);
+	console.log(req.params);
+	console.log(req.query);
+	res.send('ok;');
+});
+
 (async () => {
 	try {
-		const strava = new Strava();
-		const refresher = new RefreshToken(strava, 'STRAVA', 5000);
-		refresher.updateToken();
-
+		// const strava = new Strava();
+		// const refresher = new RefreshToken(strava, 'STRAVA', 5000);
+		// refresher.updateToken();
 		// const activities = await strava.getData();
 		// const calculator = new RunningCalculator(activities);
 		// const computedData = calculator.compute(activities);
