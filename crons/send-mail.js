9f8acf98198d8fb70bd8f936b97b89bf36acec45
amoutai <amin.moutai@gmail.com>
2024-07-24 21:20:42 +0200

    feat: adding mail

--- crons/send-mail.js
+++ crons/send-mail.js
@@ -1,13 +1,13 @@
 const StravaService = require('../src/api/Strava/StravaService');
-const Mailer = require('../src/api/Strava/StravaService');
+const Mailer = require('../src/services/Mailer');
 
 const sendMail = async () => {
 	const [data] = await StravaService.getMailData();
 	if (!data) {
 		throw new Error('No data provided');
 	}
-	/* 	const mailer = new Mailer();
-	mailer.sendMail(data); */
+	const mailer = new Mailer();
+	mailer.sendMail(data);
 	process.exit();
 };
 
--- src/services/Mailer.js
+++ src/services/Mailer.js
@@ -1,2 +1,31 @@
+const nodemailer = require('nodemailer');
 
-//TODO: wrapper for nodemailer & send mail
+class Mailer {
+	constructor() {
+		this.user = process.env.MAIL_USER;
+		this.transporter = nodemailer.createTransport({
+			host: 'smtp.gmail.com',
+			port: 465,
+			secure: true,
+			auth: {
+				user: this.user,
+				pass: process.env.MAIL_PASSWORD,
+			},
+		});
+	}
+
+	sendMail(data) {
+		return this.transporter
+			.sendMail({
+				from: this.user,
+				to: this.user,
+				subject: 'Review ✔',
+				text: data,
+				html: `<b>${data}</b>`,
+			})
+			.then((info) => console.log(`Message sent: ${info}`))
+			.catch((er) => console.error(er));
+	}
+}
+
+module.exports = Mailer;
